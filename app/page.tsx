db502056a7018c037b2e590366ecd93670755902
Nicolas Torres <desmondgamesyt@gmail.com>
2023-10-18 16:59:37 -0300

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,164 @@
+"use client";
 import Image from 'next/image'
+import { useState } from 'react';
+import { RiAiGenerate } from "react-icons/ri"
+import { BiBrush } from "react-icons/bi"
+import { LuImagePlus } from "react-icons/lu"
+import { PiPaperPlaneRightDuotone } from "react-icons/pi"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  const [mode, setMode] = useState<boolean>(false); // false = generate, true = edit
+  const [image, setImage] = useState<{ file: File | null, url: string }>({
+    file: null,
+    url: ""
+  });
+  const [currentPrompt, setCurrentPrompt] = useState<{ prompt: string, active: boolean }>({
+    prompt: "",
+    active: true
+  })
+  const [promps, setPrompts] = useState<Array<{ promp: string, index: number, response: boolean, responseImage: string | null, loadingPropmt: boolean }>>([])
+
+  const generateImage = async (prompt: string): Promise<void> => {
+    setTimeout(() => {
+      setPrompts((prev) => {
+        const newArray = [...prev]
+        newArray.push({ promp: "generating your " + prompt + "....", index: promps.length, response: true, responseImage: null, loadingPropmt: true })
+        return newArray
+      })
+    },500)
+    const response = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer 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",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+      })
+    })
+    const data = await response.json() ?? null;
+    if (!response.ok) {
+      if(data && data.error){
+        if(data.error.code === "content_policy_violation"){
+          toast.error("You prompt contains content that violates openAI content policy, please try again with a different prompt!")
+        }
+      }else{
+        toast.error("Something went wrong, please try again!")
+      }
+      setPrompts((prev) => {
+        const array = [...prev]
+        array.pop()
+        return array;
+      })
+      setCurrentPrompt((prev) => ({ ...prev, prompt: "", active: true }))
+      return;
+    }
+    setPrompts((prev) => {
+      var arrayToModify = [...prev]
+      arrayToModify[arrayToModify.length - 1].promp = arrayToModify[arrayToModify.length - 1].promp.replace("generating your ", "Here's your prompt: ")
+      arrayToModify[arrayToModify.length - 1].responseImage = data?.data[0]?.url
+      arrayToModify[arrayToModify.length - 1].loadingPropmt = false
+      return arrayToModify
+    })
+    setCurrentPrompt((prev) => ({ ...prev, prompt: "", active: true }))
+    return;
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className='bg-[#fafafa] w-full h-screen'>
+      <ToastContainer />
+      <div className='bg-[#343541] flex items-center justify-center h-screen w-full overflow-hidden'>
+        <div className={`${mode ? "w-[40%]" : " w-[0px]"} h-full text-white bg-[#202123] overflow-hidden`} style={{ transition: "width 700ms ease-in-out" }}>
+          sasdfgdsfgsdfg
+        </div>
+        <div className={`${mode ? "w-[60%] " : "w-full"} relative h-full flex justify-center`} style={{ transition: "width 700ms ease-in-out" }}>
+          {promps.length > 0 && <span id='generate' className='z-10 text-gray-300 text-sm  top-0 py-4 bg-[#343541] left-0  fixed flex items-center justify-center'>Generate</span>}
+          <div className='z-50 absolute left-2/4 -translate-x-2/4 top-12 flex items-center text-white '>
+
+            <div className={`${promps.length > 0 ? "opacity-0 pointer-events-none" : "opacity-100"} transition-opacity duration-300 relative bg-[#202123] flex items-center rounded-xl p-1 w-fit child:w-[150px] child:py-3 child:flex child:items-center child:justify-center`}>
+
+              <div className={` ${mode ? "translate-x-[99%]" : "translate-x-[1%]"} transition-all bg-[#40414f] absolute  top-2/4 -translate-y-2/4  h-[85%] w-[95%] rounded-xl`}>
+
+              </div>
+              <div onClick={() => setMode(false)} className={`${promps.length === 0 ? "opacity-100" : "opacity-0 pointer-events-none"} ${mode ? "opacity-50" : "opacity-100"} rounded-xl cursor-pointer z-10 transition-opacity`}>
+                <span>
+                  <RiAiGenerate className='inline-block mr-2 text-green-400' />
+                  Generate
+                </span>
+              </div>
+              <div onClick={() => setMode(true)} className={`${promps.length === 0 ? "opacity-100" : "opacity-0 pointer-events-none"} ${mode ? "opacity-100" : "opacity-50"} rounded-xl cursor-pointer relative z-10 transition-opacity`}>
+                <BiBrush className='inline-block mr-2 text-purple-400' />
+                <span >Edit</span>
+              </div>
+            </div>
+          </div>
+          <h1 className={`${promps.length > 0 ? "opacity-0" : "opacity-100"} transition-opacity duration-300 font-bold text-[#565869] text-2xl absolute left-2/4 -translate-x-2/4 top-56`}>FunPic AI</h1>
+          <div className=' z-1 w-full relative h-screen flex flex-col '>
+            <div className='before:fixed before:bg-opacity-50 before:w-full before:bg-[#343541] before:bottom-0  before:py-2 before:content-[""] pt-16 pb-20 relative h-full overflow-y-scroll  child:min-h-[100px] child:flex child:items-center child:justify-center'>
+              <div className='w-full py-3 flex-col'>
+                {
+                  promps.map((promps, index) => {
+                    return (
+
+                      <div className={`${promps.response ? "bg-[#444654]" : ""} w-full`}>
+                        <div key={index} className={`flex items-start gap-4 w-[500px] h-fit py-4 mx-auto`}>
+
+                          <img className='w-10 h-10 rounded-full' src={`${promps.response ? "https://avatars.githubusercontent.com/u/59100281?v=4" : "https://i.ibb.co/DCyLFGS/Captura-de-pantalla-2023-10-18-145501.png"} `} alt='user-icon' />
+
+                          <div className='w-full'>
+                            <p className={`${promps.loadingPropmt ? "tilt" : ""} break-words text-slate-200 text-sm leading-8`}>
+                              {promps.promp}
+                            </p>
+                            {
+                              promps.responseImage ? <><br></br><img className='w-52 h-52' src={promps.responseImage} alt='response' /> </> : undefined
+                            }
+                          </div>
+                        </div>
+                      </div>
+
+                    )
+                  })
+                }
+              </div>
+            </div>
+            <div className='z-10 before:w-full before:h-full before:absolute before:bg-black before:blur-xl before:scale-x-150 before:bg-opacity-[50%] before:content-[""] absolute bottom-0 py-5 flex items-center justify-center text-white w-full'>
+              <div className='flex items-center justify-center gap-4 w-full'>
+                <div className='text-2xl text-gray-300 cursor-pointer z-20'>
+                  <LuImagePlus onClick={() => document.getElementById("add-image")?.click()} />
+                  <input
+                    id='add-image'
+                    type="file"
+                    className="hidden"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      const file: File | null = e.target.files && e.target.files[0] ? e.target.files[0] : null;
+                      if (file) {
+                        const url = URL.createObjectURL(file);
+                        setImage({ file, url });
+                      }
+                    }}
+                  />
+                </div>
+                <div className='relative w-full max-w-[500px]'>
+                  <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setCurrentPrompt((prev) => ({ ...prev, prompt: e.target.value }))
+                  }} value={currentPrompt.prompt} type='text' className={`${currentPrompt.active ? "" : "cursor-not-allowed brightness-50"} bg-[#40414f] text-white p-2 rounded-xl shadow-md w-full`} placeholder='Enter a prompt to generate an image' />
+                  <PiPaperPlaneRightDuotone className={`${currentPrompt.active ? "" : "cursor-not-allowed pointer-events-none"} absolute right-2 top-2/4 -translate-y-2/4 text-gray-300 z-10 cursor-pointer`} onClick={() => {
+                    if (currentPrompt.prompt.length === 0) return toast.error("Please enter a prompt!")
+                    setPrompts([...promps, { promp: currentPrompt.prompt, index: promps.length, response: false, responseImage: null, loadingPropmt: false }])
+                    setCurrentPrompt({
+                      prompt: "",
+                      active: false
+                    })
+                    generateImage(currentPrompt.prompt)
+                  }} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+    </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
