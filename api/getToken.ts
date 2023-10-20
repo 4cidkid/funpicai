dc6194fa2da5b4cc5f67ca081504bfaf5b20f4f8
Nicolas Torres <desmondgamesyt@gmail.com>
2023-10-19 23:48:40 -0300

    modified body of getToken fetch

--- api/getToken.ts
+++ api/getToken.ts
@@ -5,7 +5,7 @@ export default async function getToken(apiKey: string): Promise<{ isOk: boolean,
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            apiKey: apiKey
+            apiKey
         })
     })
     let data;
