8de1cb73441ad3ec4ed844d2c6df3e50f1f22b1f
Nicolas Torres <desmondgamesyt@gmail.com>
2023-10-19 23:49:52 -0300

    added width & height to Image components

--- components/chatLabel.tsx
+++ components/chatLabel.tsx
@@ -6,14 +6,14 @@ export default function ChatLabel({ prompt, tilt,index }: { prompt: Prompts[0], 
         <div key={index} className={`${prompt.response ? "bg-[#444654]" : ""} w-full`}>
             <div className={`flex items-start gap-4 w-[500px] h-fit py-4 mx-auto`}>
 
-                <Image className='w-10 h-10 rounded-full' src={`${prompt.response ? "/bender.png" : "/calamardo.png"} `} alt='user-icon' />
+                <Image width={50} height={50} className=' rounded-full' src={`${prompt.response ? "/bender.jpeg" : "/calamardo.png"} `} alt='user-icon' />
 
                 <div className='w-full'>
                     <p className={`${prompt.loadingPropmt ? tilt : ""} break-words text-slate-200 text-sm leading-8`}>
                         {prompt.promp}
                     </p>
                     {
-                        prompt.responseImage ? <><br></br><Image className='w-52 h-52' src={prompt.responseImage} alt='response' /> </> : undefined
+                        prompt.responseImage ? <><br></br><Image width={208} height={208} className='w-52 h-52' src={prompt.responseImage} alt='response' /> </> : undefined
                     }
                 </div>
             </div>
