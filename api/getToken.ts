ee5b009b429f0813b8c03b35719dcb395311a639
Nicolas Torres <desmondgamesyt@gmail.com>
2023-10-19 22:40:28 -0300

    added function to get token from api

---
+++ api/getToken.ts
@@ -0,0 +1,36 @@
+export default async function getToken(apiKey: string): Promise<{ isOk: boolean, token: string | null, message: string | null }> {
+    const response = await fetch("/api/set-key", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            apiKey: apiKey
+        })
+    })
+    let data;
+    try {
+        data = await response.json()
+    } catch (err) {
+        return {
+            isOk: false,
+            token: null,
+            message: "An error ocurred parsing the response"
+        }
+    }
+    const { message, token } = data;
+    if (!response.ok) {
+        return {
+            isOk: false,
+            message,
+            token: null
+        }
+    } else {
+        return {
+            isOk: true,
+            token,
+            message: null
+        }
+    }
+
+}
