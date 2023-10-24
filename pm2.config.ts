503dbd6b6df11d7cc387a7fab35bee6f7c3e6ef0
Nicolas Torres <desmondgamesyt@gmail.com>
2023-10-24 01:47:01 -0300

    added pm2 production file

---
+++ pm2.config.ts
@@ -0,0 +1,20 @@
+const path = require('path');
+const rootPath = path.dirname(path.dirname(__filename));
+
+module.exports = {
+    apps: [
+        {
+            name: 'funpicai',
+            cwd: rootPath,
+            script: 'node_modules/next/dist/bin/next',
+            args: 'start',
+            instances: 0,
+            exec_mode: 'cluster',
+            watch: false,
+            merge_logs: true,
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
+};
