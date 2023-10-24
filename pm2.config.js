92faab82ffde7aa1866dd76eba099d9c96519b88
Nicolas Torres <desmondgamesyt@gmail.com>
2023-10-24 11:43:32 -0300

    modified pm2 script

--- pm2.config.js
+++ pm2.config.js
@@ -1,20 +1,9 @@
-const path = require('path');
-const rootPath = path.dirname(path.dirname(__filename));
-
 module.exports = {
     apps: [
-        {
-            name: 'funpicai',
-            cwd: rootPath,
-            script: 'node_modules/next/dist/bin/next',
-            args: 'start',
-            instances: 2,
-            exec_mode: 'cluster',
-            watch: false,
-            merge_logs: true,
-            env: {
-                NODE_ENV: 'production'
-            }
-        }
-    ]
-};
+      {
+        name: "my-app",
+        script: "next",
+        instances: 2,
+      },
+    ],
+  };
