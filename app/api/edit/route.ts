64fb333befbcfaa7412fe1c71df2584f3e5f610e
Nicolas Torres <desmondgamesyt@gmail.com>
2023-10-27 12:44:59 -0300

    modified post route to be more type-safe

--- app/api/edit/route.ts
+++ app/api/edit/route.ts
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import processImage from "@/utils/processImage";
 import resizeImage from "@/utils/resizeImage";
 import jwt, { JwtPayload } from "jsonwebtoken";
-
+import { EditData } from "@/types/types";
 export async function POST(request: NextRequest, response: NextResponse) {
-    let data;
+    let data: EditData;
     try {
-        data = await request.formData()
+        data = await request.formData() as unknown as EditData;
     } catch (err) {
         return NextResponse.json({
             message: "Error getting data from form"
@@ -14,13 +14,28 @@ export async function POST(request: NextRequest, response: NextResponse) {
             status: 500
         })
     }
-    const mask = data.get("mask")
-    const originalImage = data.get("image");
-    const prompt = data.get("prompt")
-    const apiKey = String(data.get("token"))
+    if (!data) {
+        return NextResponse.json({
+            message: "Error getting data from form"
+        }, {
+            status: 500
+        })
+    }
+    let mask: File | null = data.get("mask") as File | null;
+    let originalImage: File | null = data.get("image") as File | null;
+    const prompt: string | null = data.get("prompt") as string | null;
+    const apiKey: string | null = data.get("token") as string | null;
+    if (!mask || !originalImage || !prompt || !apiKey) {
+        console.log("Error getting data from form", data)
+        return NextResponse.json({
+            message: "There was an error trying to get all the necessary information"
+        }, {
+            status: 400
+        });
+    }
     let apiKeyDecoded: JwtPayload;
     try {
-        apiKeyDecoded = jwt.verify(apiKey, String(process.env.SECRET_KEY)) as JwtPayload
+        apiKeyDecoded = jwt.verify(apiKey.toString(), String(process.env.SECRET_KEY)) as JwtPayload
     } catch (err) {
         console.error(err)
         return NextResponse.json({
@@ -30,52 +45,51 @@ export async function POST(request: NextRequest, response: NextResponse) {
         })
     }
     const apiKeyFromToken = apiKeyDecoded.apiKey.apiKey;
-    if (!prompt || !apiKeyFromToken || !mask || !originalImage) {
+    if (!apiKeyFromToken) {
         return NextResponse.json({
             message: "There was an error trying to get all the necessary information"
         }, {
             status: 400
         });
     }
-    if (mask instanceof File && originalImage instanceof File) {
-        try {
-            const widthAndHeight = 1080;
-            const resizeMask = await resizeImage(mask, widthAndHeight, widthAndHeight);
-            const resizeOriginalImageBuffer = await resizeImage(originalImage, widthAndHeight, widthAndHeight)
-            const maskProcessedBlob = await processImage(resizeMask)
-            const originalImageBlob = new Blob([resizeOriginalImageBuffer], { type: 'image/png' });
-            const formData = new FormData()
-            formData.append("prompt", prompt)
-            formData.append("mask", maskProcessedBlob)
-            formData.append("image", originalImageBlob)
 
-            const response = await fetch("https://api.openai.com/v1/images/edits", {
-                method: "POST",
-                headers: {
-                    "Authorization": `Bearer ${apiKeyFromToken}`,
-                },
-                body: formData
-            })
-            const editedImage = await response.json()
+    try {
+        const widthAndHeight = 1080;
+        const resizeMask = await resizeImage(mask, widthAndHeight, widthAndHeight);
+        const resizeOriginalImageBuffer = await resizeImage(originalImage, widthAndHeight, widthAndHeight)
+        const maskProcessedBlob = await processImage(resizeMask)
+        const originalImageBlob = new Blob([resizeOriginalImageBuffer], { type: 'image/png' });
+        const formData = new FormData()
+        formData.append("prompt", prompt)
+        formData.append("mask", maskProcessedBlob)
+        formData.append("image", originalImageBlob)
+
+        const response = await fetch("https://api.openai.com/v1/images/edits", {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${apiKeyFromToken}`,
+            },
+            body: formData
+        })
+        const editedImage = await response.json()
+        if (!response.ok) {
+            const message: string = editedImage.error.code?.toLowerCase() === 'invalid_api_key' ? "Invalid API key" : "There was an error processing the image"
             return NextResponse.json({
-                message: "Image successfully edited",
-                image: editedImage.data[0].url
-            })
-        } catch (err) {
-            console.error(err)
-            return NextResponse.json({
-                message: "An error ocurred proccesing the image"
+                message: message
             }, {
                 status: 500
             })
         }
-
-
-    } else {
         return NextResponse.json({
-            message: "Form data is incorrect"
+            message: "Image successfully edited",
+            image: editedImage.data[0].url
+        })
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json({
+            message: "An error ocurred proccesing the image"
         }, {
-            status: 400
+            status: 500
         })
     }
 
